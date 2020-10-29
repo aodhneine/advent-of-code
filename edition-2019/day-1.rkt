c5d2e4fe3c1bce83226d2c572eb172246c9969eb
aodhneine <aodhneine@protonmail.ch>
2020-10-29 21:48:46 +0000

    Add solution for the second part of day one

--- edition-2019/day-1.rkt
+++ edition-2019/day-1.rkt
@@ -1,13 +1,20 @@
 #lang racket
 
-(define (fuel-requirement module-mass)
-  (- (floor (/ module-mass 3)) 2))
+(define (fuel-requirement mass)
+  (- (floor (/ mass 3)) 2))
+
+(define (module-fuel-requirement mass acc)
+  (let ([required-mass (fuel-requirement mass)])
+    (cond
+      [(and (<= required-mass 2) (> required-mass 0)) (+ acc required-mass)]
+      [(< required-mass 0) acc]
+      [else (module-fuel-requirement required-mass (+ acc required-mass))])))
 
 (define (sum lst)
   (foldl (lambda (x a) (+ a x)) 0 lst))
 
 (define (total-fuel-requirement modules)
-  (sum (map fuel-requirement modules)))
+  (sum (map (lambda (x) (module-fuel-requirement x 0)) modules)))
 
 (define (load-input file)
   (let ([input (open-input-file file #:mode 'text)])
